990469812a8e3df2fa71ffc4aa17cea676fa8e4c
Stefanie M <stefanielynn9008@gmail.com>
2024-01-21 09:01:51 -0800

    Added API integration, updated JS

--- src/script.js
+++ src/script.js
@@ -1,12 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
+  console.log("poem generated");
   new Typewriter("#poem", {
-    strings: "Each thought of you fills me with sweet emotion",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "ta59o034a54d51cbbddfb3328040f28f";
+  let context =
+    "You are a romantic poem writing expert, and love to write short poems. Your mission is to generate a 4 line in basic HTML. Make sure to follow the user instructions. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem. Do not include a poem title";
+  let prompt = `User instructions: Generate a love poem about ${instructionsInput.value}`;
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  console.log("Generating poem");
+  console.log(`Prompt: ${prompt}`);
+  console.log(`Context: ${context}`);
+
+  axios.get(apiUrl).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
